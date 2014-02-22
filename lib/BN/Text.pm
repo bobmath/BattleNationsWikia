9188755a8efbed5aafe42259b2c9e4f03e74092a
Bob Mathews <bobmath@sbcglobal.net>
2014-02-21 23:17:59 -0800

    fetch text or nothing

--- lib/BN/Text.pm
+++ lib/BN/Text.pm
@@ -4,17 +4,27 @@ use warnings;
 
 my $text;
 
+sub load_text {
+   return if $text;
+   $text = BN::JSON->read('BattleNations_en.json');
+   my $delta = BN::JSON->read('Delta_en.json');
+   while (my ($k,$v) = each %$delta) {
+      $text->{$k} = $v;
+   }
+}
+
 sub get {
    my ($class, $key) = @_;
    return 'none' unless defined $key;
-   unless ($text) {
-      $text = BN::JSON->read('BattleNations_en.json');
-      my $delta = BN::JSON->read('Delta_en.json');
-      while (my ($k,$v) = each %$delta) {
-         $text->{$k} = $v;
-      }
-   }
+   load_text() unless $text;
    return $text->{lc($key)} // $text->{$key} // $key;
+}
+
+sub fetch {
+   my ($class, $key) = @_;
+   return undef unless defined $key;
+   load_text() unless $text;
+   return $text->{lc($key)};
 }
 
 1 # end BN::Text
