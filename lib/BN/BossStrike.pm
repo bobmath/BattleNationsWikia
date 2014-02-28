6db78b42652f38e954bb68d7971110abf677cd52
Bob Mathews <bobmath@sbcglobal.net>
2014-02-27 22:57:10 -0800

    explicit list of ignored missions

--- lib/BN/BossStrike.pm
+++ lib/BN/BossStrike.pm
@@ -5,11 +5,27 @@ use warnings;
 my $strikes;
 my $json_file = 'CoopBossEventConfig.json';
 
+my %old_strikes;
+$old_strikes{$_} = 1 foreach qw{
+   boss0_event_raiders
+   boss2_event_raiders_QA
+   boss3_event_SW_QA
+   boss4_event_zombies_QA
+   boss5_event_SW_Animals_QA
+   boss6_event_Rebels_Airplanes_QA
+   boss7_event_SilverWolves2_QA
+   coopBossEvent_integ_test_1
+   coopBossEvent_integ_test_achieve
+   coopBossEvent_integ_test_locked
+   coopBossEvent_manual_test_1
+   qa_functionality_test_event
+};
+
 sub all {
    my ($class) = @_;
    $strikes ||= BN::JSON->read($json_file);
    return map { $class->get($_) }
-      sort grep { !/_qa|_test|boss0/i } keys %$strikes;
+      sort grep { !$old_strikes{$_} } keys %$strikes;
 }
 
 sub get {
