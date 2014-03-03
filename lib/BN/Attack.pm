1fe97766656c0a67678d18c63c0e99a22bdaceca
Bob Mathews <bobmath@sbcglobal.net>
2014-03-02 21:36:46 -0800

    clamp crit to 100%

--- lib/BN/Attack.pm
+++ lib/BN/Attack.pm
@@ -199,6 +199,7 @@ sub crit {
    if (my $mods = $att->{criticalBonuses}) {
       foreach my $targ (sort keys %$mods) {
          my $val = $crit + $mods->{$targ};
+         $val = 100 if $val > 100;
          my $targname = $critmap{$targ} || $targ;
          push @crit, "$val% vs. $targname";
       }
