89d64b91f7d55f8b4882a8a5d6259717be4bd8b3
Bob Mathews <bobmath@sbcglobal.net>
2014-03-16 17:46:59 -0700

    xp

--- lib/BN/Out/Guilds.pm
+++ lib/BN/Out/Guilds.pm
@@ -1,6 +1,7 @@
 package BN::Out::Guilds;
 use strict;
 use warnings;
+use Data::Dump qw( dump );
 
 sub write {
    my $guilds = BN::File->json('GuildConfig.json');
@@ -8,16 +9,19 @@ sub write {
    open my $F, '>', $file or die "Can't write $file: $!";
 
    print $F qq({| class="wikitable standout"\n);
-   print $F "|-\n! Guild Level !! Member Limit !! XP Bonus !! SP Bonus\n";
+   print $F "|-\n! Guild Level !! XP Required !! Member Limit",
+      " !! XP Bonus !! SP Bonus\n";
    my $levels = $guilds->{guildLevelProperties} or die;
    foreach my $lev (sort { $a <=> $b } keys %$levels) {
       my $level = $levels->{$lev} or die;
+      my $xp = BN->commify($level->{xpRequirement});
       print $F qq(|- align="center"\n! $lev\n);
-      print $F "| $level->{memberCap} || $level->{xpBoost}% ",
+      print $F "| $xp || $level->{memberCap} || $level->{xpBoost}% ",
          "|| $level->{spBoost}%\n";
    }
-   print $F "|}\n";
+   print $F "|}\n\n";
 
+   print $F dump($guilds);
    close $F;
    BN::Out->checksum($file);
 }
