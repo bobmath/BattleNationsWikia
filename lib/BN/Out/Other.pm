807c65f0a049c1651383730fa07c71fbe5509bc5
Bob Mathews <bobmath@sbcglobal.net>
2014-04-14 06:16:49 -0700

    boosts

--- lib/BN/Out/Other.pm
+++ lib/BN/Out/Other.pm
@@ -1,8 +1,14 @@
 package BN::Out::Other;
 use strict;
 use warnings;
+use Data::Dump qw( dump );
 
 sub write {
+   write_hints();
+   write_boosts();
+}
+
+sub write_hints {
    my $hints = BN::File->json('My_Land_Hint.json');
    my $file = BN::Out->filename('info', 'Loading Screen Statements');
    open my $F, '>', $file or die "Can't write $file: $!\n";
@@ -15,6 +21,15 @@ sub write {
       }
    }
 
+   close $F;
+   BN::Out->checksum($file);
+}
+
+sub write_boosts {
+   my $boosts = BN::File->json('RewardMultiplierOffers.json');
+   my $file = BN::Out->filename('info', 'Boosts');
+   open my $F, '>', $file or die "Can't write $file: $!\n";
+   print $F dump($boosts);
    close $F;
    BN::Out->checksum($file);
 }
