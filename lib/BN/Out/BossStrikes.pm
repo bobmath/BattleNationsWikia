637b6e5f043914276fc6b031f2402790ec68c399
Bob Mathews <bobmath@sbcglobal.net>
2014-04-23 16:53:13 -0700

    extract methods

--- lib/BN/Out/BossStrikes.pm
+++ lib/BN/Out/BossStrikes.pm
@@ -7,67 +7,74 @@ sub write {
    foreach my $strike (BN::BossStrike->all()) {
       my $file = BN::Out->filename('strikes', $strike->name());
       open my $F, '>', $file or die "Can't write $file: $!";
-
-      my @tiers = $strike->tiers();
-      push @tiers, $tiers[-1]->extend() for 1..2;
-
-      print $F qq(==Rewards==\n{| class="wikitable standout"\n);
-      print $F "|-\n! Tier !! Rewards !! Points awarded",
-         " !! Points to earn !! Total points\n";
-      my $sum;
-      foreach my $tier (@tiers) {
-         my $rewards = BN->format_amount($tier->rewards()) || '-';
-         my $award = join('', BN->format_amount($tier->cost()), " &rarr; ",
-            '{{BSPoints|', BN->commify($tier->points_awarded()), '}}');
-         my $pts = $tier->points_needed();
-         $sum += $pts;
-         print $F qq{|- align="center"\n};
-         print $F '! ', $tier->tier(), "\n";
-         print $F '| ', join(' || ', $rewards, $award,
-            BN->commify($pts), BN->commify($sum)), "\n";
-      }
-      print $F "|}\n\n";
-
-      my %encounters;
-      foreach my $encounter (@{$strike->{globalEventEncounters}}) {
-         $encounters{$encounter->{encounterId}} = 1;
-      }
-      foreach my $tier ($strike->tiers()) {
-         foreach my $encounter (@{$tier->{encounters}}) {
-            $encounters{$encounter->{encounterId}} = 1;
-         }
-      }
-
-      my %units;
-      foreach my $key (keys %encounters) {
-         my $encounter = BN::Encounter->get($key) or next;
-         foreach my $unit ($encounter->units()) {
-            $units{$unit} = 1;
-         }
-      }
-
-      my %names;
-      foreach my $key (keys %units) {
-         my $unit = BN::Unit->get($key) or next;
-         $names{$unit->wikilink()} = 1;
-      }
-
-      my @names = sort keys %names;
-      my $break = 0;
-      print $F qq(==Enemies=={| width="100%"\n|-\n);
-      foreach my $i (0 .. $#names) {
-         if ($i >= $break) {
-            print $F "|\n";
-            $break += int((@names+2)/3);
-         }
-         print $F "*$names[$i]\n";
-      }
-      print $F "|}\n\n";
-
+      show_tiers($F, $strike);
+      show_enemies($F, $strike);
       print $F dump($strike), "\n";
       close $F;
       BN::Out->checksum($file);
    }
+}
+
+sub show_tiers {
+   my ($F, $strike) = @_;
+   my @tiers = $strike->tiers();
+   push @tiers, $tiers[-1]->extend() for 1..2;
+
+   print $F qq(==Rewards==\n{| class="wikitable standout"\n);
+   print $F "|-\n! Tier !! Rewards !! Points awarded",
+      " !! Points to earn !! Total points\n";
+   my $sum;
+   foreach my $tier (@tiers) {
+      my $rewards = BN->format_amount($tier->rewards()) || '-';
+      my $award = join('', BN->format_amount($tier->cost()), " &rarr; ",
+         '{{BSPoints|', BN->commify($tier->points_awarded()), '}}');
+      my $pts = $tier->points_needed();
+      $sum += $pts;
+      print $F qq{|- align="center"\n};
+      print $F '! ', $tier->tier(), "\n";
+      print $F '| ', join(' || ', $rewards, $award,
+         BN->commify($pts), BN->commify($sum)), "\n";
+   }
+   print $F "|}\n\n";
+}
+
+sub show_enemies {
+   my ($F, $strike) = @_;
+   my %encounters;
+   foreach my $encounter (@{$strike->{globalEventEncounters}}) {
+      $encounters{$encounter->{encounterId}} = 1;
+   }
+   foreach my $tier ($strike->tiers()) {
+      foreach my $encounter (@{$tier->{encounters}}) {
+         $encounters{$encounter->{encounterId}} = 1;
+      }
+   }
+
+   my %units;
+   foreach my $key (keys %encounters) {
+      my $encounter = BN::Encounter->get($key) or next;
+      foreach my $unit ($encounter->units()) {
+         $units{$unit} = 1;
+      }
+   }
+
+   my %names;
+   foreach my $key (keys %units) {
+      my $unit = BN::Unit->get($key) or next;
+      $names{$unit->wikilink()} = 1;
+   }
+
+   my @names = sort keys %names;
+   my $break = 0;
+   print $F qq(==Enemies==\n{| width="100%"\n|-\n);
+   foreach my $i (0 .. $#names) {
+      if ($i >= $break) {
+         print $F "|\n";
+         $break += int((@names+2)/3);
+      }
+      print $F "*$names[$i]\n";
+   }
+   print $F "|}\n\n";
 }
 
 1 # end BN::Out::BossStrikes
