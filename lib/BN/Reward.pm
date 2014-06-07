4808e43620e89691703d7801d90d112aeb0bd758
Bob Mathews <bobmath11@icloud.com>
2014-06-07 14:52:36 -0700

    more detailed reward analysis

--- lib/BN/Reward.pm
+++ lib/BN/Reward.pm
@@ -43,3 +43,137 @@ sub format_chance {
 }
 
 1 # end BN::Reward
+__END__
+
+sub get_rewards {
+   my ($tag) = @_;
+   return unless $tag;
+   my $rewards = $reward_map->{$tag} or return;
+   my $list = $rewards->{list} or return;
+   my @rewards;
+
+   foreach my $item (@$list) {
+      my $levels = $item->{levels};
+      my $lmin = $levels->{min} || 1;
+      my $lmax = $levels->{max} || 65;
+      $lmax = 65 if $lmax > 65;
+
+      if (my $money = $item->{money}) {
+         push @rewards, new_reward(rsrc => 'money', $money, $lmin, $lmax);
+      }
+
+      if (my $rsrc = $item->{resources}) {
+         foreach my $key (sort keys %$rsrc) {
+            push @rewards,
+               new_reward(rsrc => $key, $rsrc->{$key}, $lmin, $lmax);
+         }
+      }
+
+      if (my $units = $item->{units}) {
+         foreach my $key (sort keys %$units) {
+            push @rewards,
+               new_reward(unit => $key, $units->{$key}, $lmin, $lmax);
+         }
+      }
+
+      if (my $ref = $item->{rewardsRefCount}) {
+         my $range = $ref->{range} or next;
+         my $min = $range->{min} or next;
+         my $max = $range->{max} or next;
+         my $mean = ($min + $max) / 2;
+         my $chance = $ref->{percent} || $ref->{chance} || 100;
+         $chance = 100 if $chance > 100;
+         $chance /= 100;
+         my $ref_rewards = get_rewards($item->{rewardsRef}) or next;
+         foreach my $ref_item (@$ref_rewards) {
+            next if $ref_item->{lmax} < $lmin || $ref_item->{lmin} > $lmax;
+            $ref_item->{lmin} = $lmin if $ref_item->{lmin} < $lmin;
+            $ref_item->{lmax} = $lmax if $ref_item->{lmax} > $lmax;
+            $ref_item->{min} *= $min;
+            $ref_item->{max} *= $max;
+            $ref_item->{mean} *= $mean;
+            $ref_item->{pct} *= $chance;
+            push @rewards, $ref_item;
+         }
+      }
+   }
+
+   return \@rewards;
+}
+
+sub new_reward {
+   my ($type, $id, $info, $lmin, $lmax) = @_;
+   my $range = $info->{range};
+   my $min = $range->{min};
+   my $max = $range->{max};
+   my $pct = $info->{percent} || $info->{chance} || 100;
+   $pct = 100 if $pct > 100;
+   return {
+      type => $type,
+      kind => $id,
+      pct => $pct,
+      min => $min,
+      max => $max,
+      mean => ($min + $max) / 2,
+      lmin => $lmin,
+      lmax => $lmax,
+   };
+}
+
+sub merge_rewards {
+   my ($in) = @_;
+   my %index;
+   foreach my $item (@$in) {
+      my $key = "$item->{type} $item->{kind}";
+      $index{$key}{$item->{lmin}-1} = 1;
+      $index{$key}{$item->{lmax}} = 1;
+   }
+
+   foreach my $key (keys %index) {
+      $index{$key} = [ sort {$a<=>$b} keys %{$index{$key}} ];
+   }
+
+   my @temp;
+   foreach my $item (sort { $a->{lmin} <=> $b->{lmin}
+      || $a->{lmax} <=> $b->{lmax} || $a->{kind} cmp $b->{kind} } @$in)
+   {
+      my @split = grep { $_ >= $item->{lmin} && $_ < $item->{lmax} }
+         @{$index{"$item->{type} $item->{kind}"}};
+      my $low = $item->{lmin};
+      foreach my $split (@split) {
+         push @temp, { %$item, lmin => $low, lmax => $split };
+         $low = $split + 1;
+      }
+      push @temp, { %$item, lmin => $low };
+   }
+
+   %index = ();
+   my @out;
+   foreach my $item (@temp) {
+      my $key = "$item->{type} $item->{kind} $item->{lmin} $item->{lmax}";
+      if (my $old = $index{$key}) {
+         my $pct = $old->{pct} + $item->{pct}
+            - $old->{pct} * $item->{pct} / 100;
+         if ($item->{pct} < 100) {
+            $old->{min} = $item->{min}
+               if $old->{pct} < 100 && $item->{min} < $old->{min};
+         }
+         elsif ($old->{pct} < 100) {
+            $old->{min} = $item->{min};
+         }
+         else {
+            $old->{min} += $item->{min};
+         }
+         $old->{mean} = ($old->{pct} * $old->{mean}
+            + $item->{pct} * $item->{mean}) / $pct;
+         $old->{max} += $item->{max};
+         $old->{pct} = $pct;
+      }
+      else {
+         $index{$key} = $item;
+         push @out, $item;
+      }
+   }
+   return \@out;
+}
+
